3db31315a908902ed67c6112eee66e617f0fb129
ankikkos <ioannou_andreas@hotmail.gr>
2017-02-08 13:03:40 +0000

    feat: added intents for opening applications on click

--- app/src/main/java/com/kikkos/myappportfolio/MainActivity.java
+++ app/src/main/java/com/kikkos/myappportfolio/MainActivity.java
@@ -1,5 +1,6 @@
 package com.kikkos.myappportfolio;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -30,46 +31,83 @@ public class MainActivity extends AppCompatActivity {
         btnShowCapstone = (Button) findViewById(R.id.btnShowCapstone);
 
         // Configuring the onClick Listeners for each Button.
-        // Configure the pop up message for each button when is clicked.
+        // Configure the intent and a pop up message for each button when is clicked.
+        // If the package manager does not find the app then an error popup message will display.
         btnShowPopularMovies.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(v.getContext(), getString(R.string.msgBtnShowPopularMovies), Toast.LENGTH_LONG).show();
+                Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.kikkos.myMovieNews");
+                if (launchIntent != null) {
+                    Toast.makeText(getApplicationContext(), getString(R.string.msgBtnShowPopularMovies), Toast.LENGTH_SHORT).show();
+                    startActivity(launchIntent);
+                } else {
+                    Toast.makeText(getApplicationContext(), getString(R.string.errorMsgAppNotFound), Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
         btnShowStockHawk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(v.getContext(), getString(R.string.msgBtnShowStockHawk), Toast.LENGTH_LONG).show();
+                Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.sam_chordas.android.stockhawk");
+                if (launchIntent != null) {
+                    Toast.makeText(getApplicationContext(), getString(R.string.msgBtnShowStockHawk), Toast.LENGTH_SHORT).show();
+                    startActivity(launchIntent);
+                } else {
+                    Toast.makeText(getApplicationContext(), getString(R.string.errorMsgAppNotFound), Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
         btnShowBuildItBigger.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(v.getContext(), getString(R.string.msgBtnShowBuildItBigger), Toast.LENGTH_LONG).show();
+                Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.udacity.gradle.builditbigger");
+                if (launchIntent != null) {
+                    Toast.makeText(getApplicationContext(), getString(R.string.msgBtnShowBuildItBigger), Toast.LENGTH_SHORT).show();
+                    startActivity(launchIntent);
+                } else {
+                    Toast.makeText(getApplicationContext(), getString(R.string.errorMsgAppNotFound), Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
         btnShowMakeYourAppMat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(v.getContext(), getString(R.string.msgBtnShowMakeYourAppMat), Toast.LENGTH_LONG).show();
+                Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.example.xyzreader");
+                if (launchIntent != null) {
+                    Toast.makeText(getApplicationContext(), getString(R.string.msgBtnShowMakeYourAppMat), Toast.LENGTH_SHORT).show();
+                    startActivity(launchIntent);
+                } else {
+                    Toast.makeText(getApplicationContext(), getString(R.string.errorMsgAppNotFound), Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
         btnShowGoUbiquitous.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(v.getContext(), getString(R.string.msgBtnShowGoUbiquitous), Toast.LENGTH_LONG).show();
+                Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.example.android.sunshine");
+                if (launchIntent != null) {
+                    Toast.makeText(getApplicationContext(), getString(R.string.msgBtnShowGoUbiquitous), Toast.LENGTH_SHORT).show();
+                    startActivity(launchIntent);
+                } else {
+                    Toast.makeText(getApplicationContext(), getString(R.string.errorMsgAppNotFound), Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
         btnShowCapstone.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(v.getContext(), getString(R.string.msgBtnShowCapstone), Toast.LENGTH_LONG).show();
+                Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.gcna.dinevenues");
+                if (launchIntent != null) {
+                    Toast.makeText(getApplicationContext(), getString(R.string.msgBtnShowCapstone), Toast.LENGTH_SHORT).show();
+                    startActivity(launchIntent);
+                } else {
+                    Toast.makeText(getApplicationContext(), getString(R.string.errorMsgAppNotFound), Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
